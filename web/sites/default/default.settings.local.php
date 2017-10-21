8c9658c044938f48e92e1fd10ba21259203f2667
Sean Dietrich <sean.e.dietrich@gmail.com>
2017-10-21 12:32:11 -0700

    Removed infinite loop of including local file

--- web/sites/default/default.settings.local.php
+++ web/sites/default/default.settings.local.php
@@ -151,10 +151,3 @@ if (PHP_SAPI !== 'cli') {
   }
 }
 
-/**
- * If there is a local settings file, then include it
- */
-$local_settings = __DIR__ . "/settings.local.php";
-if (file_exists($local_settings)) {
-    include $local_settings;
-}
