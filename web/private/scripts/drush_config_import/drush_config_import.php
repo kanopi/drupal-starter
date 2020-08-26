6e492e729d95c439248bcc4e8937d243db4306ea
Sean Dietrich <sean.e.dietrich@gmail.com>
2020-08-26 13:24:04 -0700

    Upgraded Packages
    Upgraded Composer Packages for Drupal
    Upgraded Docksal Config
    Added Pantheon specifics

---
+++ web/private/scripts/drush_config_import/drush_config_import.php
@@ -0,0 +1,11 @@
+<?php
+
+// Import all config changes.
+echo "Importing configuration from yml files...\n";
+passthru('drush config-import -y');
+echo "Import of configuration complete.\n";
+
+//Clear all cache
+echo "Rebuilding cache.\n";
+passthru('drush cr');
+echo "Rebuilding cache complete.\n";
--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -9,7 +9,7 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  * Include the Pantheon-specific settings file.
  *
  * n.b. The settings.pantheon.php file makes some changes
- *      that affect all envrionments that this site
+ *      that affect all environments that this site
  *      exists in.  Always include this file, even in
  *      a local development environment, to ensure that
  *      the site settings remain consistent.
@@ -17,11 +17,18 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 include __DIR__ . "/settings.pantheon.php";
 
 /**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
+
+/**
  * Place the config directory outside of the Drupal root.
  */
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
-);
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
 
 /**
  * If there is a local settings file, then include it
@@ -30,9 +37,3 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-
-/**
- * Always install the 'standard' profile to stop the installer from
- * modifying settings.php.
- */
-$settings['install_profile'] = 'standard';
