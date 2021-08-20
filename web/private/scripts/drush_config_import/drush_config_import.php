318c6048574f3f26e84d4f8b9033d4e63ffb0e4f
Paul Sheldrake <paul.sheldrake@gmail.com>
2021-08-20 14:16:12 -0700

    circleci updates

--- web/private/scripts/drush_config_import/drush_config_import.php
+++ web/private/scripts/drush_config_import/drush_config_import.php
@@ -1,5 +1,10 @@
 <?php
 
+//Clear all cache
+echo "Rebuilding cache.\n";
+passthru('drush cr');
+echo "Rebuilding cache complete.\n";
+
 // Import all config changes.
 echo "Importing configuration from yml files...\n";
 passthru('drush config-import -y');
