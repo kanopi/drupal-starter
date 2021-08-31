7f5e36c2d2e4a138e479f40b55a4580a6be5a907
Paul Sheldrake <paul.sheldrake@gmail.com>
2021-08-31 15:11:43 -0700

    tweak quick silver drush commands

--- web/private/scripts/drush_config_import/drush_config_import.php
+++ web/private/scripts/drush_config_import/drush_config_import.php
@@ -5,15 +5,15 @@ echo "Rebuilding cache.\n";
 passthru('drush cr');
 echo "Rebuilding cache complete.\n";
 
-// Import all config changes.
-echo "Importing configuration from yml files...\n";
-passthru('drush config-import -y');
-echo "Import of configuration complete.\n";
-
 // Update DB
 echo "Update database...\n";
 passthru('drush updb -y');
 echo "Update database complete.\n";
+
+// Import all config changes.
+echo "Importing configuration from yml files...\n";
+passthru('drush config-import -y');
+echo "Import of configuration complete.\n";
 
 //Clear all cache
 echo "Rebuilding cache.\n";
