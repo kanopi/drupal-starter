23bb7cf0b265fce0bddb022010aaf716ce584259
Jim Birch <5177009+thejimbirch@users.noreply.github.com>
2023-07-20 16:37:49 -0400

    Update rector.php

--- rector.php
+++ rector.php
@@ -1,35 +1,35 @@
 <?php
- 
+
 declare(strict_types=1);
- 
+
 use DrupalFinder\DrupalFinder;
-use Rector\Core\Configuration\Option;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
- 
-return static function (ContainerConfigurator $containerConfigurator): void {
+use DrupalRector\Set\Drupal9SetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    // Adjust the set lists to be more granular to your Drupal requirements.
     // @todo find out how to only load the relevant rector rules.
     //   Should we try and load \Drupal::VERSION and check?
-    $containerConfigurator->import(__DIR__ .  '/vendor/palantirnet/drupal-rector/config/drupal-8/drupal-8-all-deprecations.php');
-    $containerConfigurator->import(__DIR__ .  '/vendor/palantirnet/drupal-rector/config/drupal-9/drupal-9-all-deprecations.php');
- 
-    $parameters = $containerConfigurator->parameters();
- 
+    $rectorConfig->sets([
+        Drupal9SetList::DRUPAL_9,
+    ]);
+
+    $parameters = $rectorConfig->parameters();
+
     $drupalFinder = new DrupalFinder();
     $drupalFinder->locateRoot(__DIR__);
     $drupalRoot = $drupalFinder->getDrupalRoot();
-    $parameters->set(Option::AUTOLOAD_PATHS, [
+    $rectorConfig->autoloadPaths([
         $drupalRoot . '/core',
         $drupalRoot . '/modules',
         $drupalRoot . '/profiles',
         $drupalRoot . '/themes'
     ]);
- 
-    $parameters->set(Option::SKIP, ['*/upgrade_status/tests/modules/*']);
-    $parameters->set(Option::SKIP, ['*/node_modules/*']);
-    $parameters->set(Option::FILE_EXTENSIONS, ['php', 'module', 'theme', 'install', 'profile', 'inc', 'engine']);
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
-    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
- 
+
+    $rectorConfig->skip(['*/upgrade_status/tests/modules/*']);
+    $rectorConfig->skip(['*/node_modules/*']);
+    $rectorConfig->fileExtensions(['php', 'module', 'theme', 'install', 'profile', 'inc', 'engine']);
+    $rectorConfig->importNames(true, false);
+    $rectorConfig->importShortClasses(false);
     $parameters->set('drupal_rector_notices_as_comments', true);
 };
