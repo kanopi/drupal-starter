bd6fb1d3db9910f56873051ba477d49c43902922
Jim Birch <5177009+thejimbirch@users.noreply.github.com>
2023-12-02 08:04:06 -0500

    Update system-checks.cy.js

--- tests/cypress/cypress/e2e/system-checks.cy.js
+++ tests/cypress/cypress/e2e/system-checks.cy.js
@@ -1,28 +1,16 @@
-
-describe('Check Drupal site settings', () => {
-
-  it('Default admin role should be set', () => {
+describe('Basic Drupal system checks.', () => {
+  it('Administration checks.', () => {
+    // Current user is an admin.
     cy.login()
     cy.visit('/admin/people/role-settings')
     cy.get('#edit-user-admin-role').find('option:selected').should('contain', 'Administrator')
-  })
-
-  it('Redis should be available', () => {
-    cy.login()
+    // Redis should be available.
     cy.visit('/admin/reports/redis')
     cy.get('.system-status-report .system-status-report__entry__value').should('contain', 'Connected')
-  })
-
-
-  it('Basic Drupal performance options set', () => {
-    cy.login()
+    // Basic Drupal performance options set.
     cy.visit('/admin/config/development/performance')
     cy.get('#edit-preprocess-css').should('be.checked')
     cy.get('#edit-preprocess-js').should('be.checked')
     cy.get('#edit-page-cache-maximum-age').find('option:selected').should('not.contain', 'no caching')
   })
-
-
-
-
 })
