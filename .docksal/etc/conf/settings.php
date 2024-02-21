f1452b367fbf04a380ad5cac5d5d3fa9dcadc9d7
Anne Bonham <anne.bonham@kanopistudios.com>
2024-02-21 09:20:54 -0800

    fixes theming errors for M3 chips

--- .docksal/etc/conf/settings.php
+++ .docksal/etc/conf/settings.php
@@ -67,3 +67,23 @@ $config['seckit.settings']['seckit_csrf']['origin'] = FALSE;
 // Disable CSS and JS aggregation.
 $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
+
+/**
+ * Salt for one-time login links, cancel links, form tokens, etc.
+ *
+ * This variable will be set to a random value by the installer. All one-time
+ * login links will be invalidated if the value is changed. Note that if your
+ * site is deployed on a cluster of web servers, you must ensure that this
+ * variable has the same value on each server.
+ *
+ * For enhanced security, you may set this variable to the contents of a file
+ * outside your document root, and vary the value across environments (like
+ * production and development); you should also ensure that this file is not
+ * stored with backups of your database.
+ *
+ * Example:
+ * @code
+ *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
+ * @endcode
+ */
+$settings['hash_salt'] = 'hiuehfw89e9873480-khds*';
