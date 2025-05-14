f955c0ef65e2668dafa34e86ca6aa93da09a7758
Anne Bonham <anne.bonham@kanopistudios.com>
2025-05-13 20:46:59 -0700

    fixes config location on pantheon

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -149,8 +149,10 @@ if ($_ENV['PANTHEON_ENVIRONMENT']) {
   }
 }
 
+if (!$_ENV['PANTHEON_ENVIRONMENT']) {
 // Automatically generated include for settings managed by ddev.
-$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
-if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
-  require $ddev_settings;
+  $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+  if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+    require $ddev_settings;
+  }
 }
